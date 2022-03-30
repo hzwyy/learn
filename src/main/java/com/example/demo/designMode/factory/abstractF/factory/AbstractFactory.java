80745ec6acf41941de5a757dfda627996d2806dc
yjy <yangjingyan@jrmf.com>
2022-03-30 15:27:56 +0800

    设计模式 -- 抽象工厂模式

--- src/main/java/com/example/demo/designMode/factory/abstractF/factory/AbstractFactory.java
+++ src/main/java/com/example/demo/designMode/factory/abstractF/factory/AbstractFactory.java
@@ -6,7 +6,8 @@ import com.example.demo.designMode.factory.abstractF.Color;
 /**
  * @author: YJY
  * @date: 2022/3/30 15:04
- * @description: 可以在这里面任意组装需要的行为 类似于组装衣服搭配
+ * @description: 可以在这里面任意组装需要的行为 类似于组装衣服搭配 将抽象工厂进一步抽象
+ *  可以抽象多次
  */
 public abstract class AbstractFactory {
 
